015c4a905fef373a79acdf7ca15f0d1e558d94b1
BrunoAraus <101360453+BrunoAraus@users.noreply.github.com>
2024-09-27 12:36:54 -0300

    Pruebas API KEY-APP
    Pruebas realizadas en la api y la app

--- APP-PANAP/src/app/3-pruebas/pruebas-api/pruebas-api.page.ts
+++ APP-PANAP/src/app/3-pruebas/pruebas-api/pruebas-api.page.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-pruebas-api',
   templateUrl: './pruebas-api.page.html',
   styleUrls: ['./pruebas-api.page.scss'],
 })
-export class PruebasApiPage implements OnInit {
+export class PruebasApiPage {
+  apiUrl = 'https://panapp.duckdns.org/rest/API_PRUEBA.php';
 
-  constructor() { }
+  usuario: string = '';
+  clave: string = '';
+  correo: string = '';
+  tipo: string = '';
 
-  ngOnInit() {
+  constructor(private http: HttpClient) {}
+
+  probarAPI() {
+    const body = {
+      usuario: this.usuario,
+      clave: this.clave,
+      correo: this.correo,
+      tipo: this.tipo,
+    };
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': ''
+    });
+
+    this.http.post(this.apiUrl, body, { headers: headers })
+      .subscribe(response => {
+        console.log('Respuesta de la API:', response);
+      }, error => {
+        console.error('Error al consumir la API:', error);
+      });
   }
-
 }
