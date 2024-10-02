70261c641b94754846a29da6b74a61a27b90f78e
BrunoAraus <101360453+BrunoAraus@users.noreply.github.com>
2024-10-02 12:59:09 -0300

    Registro de usuario Backend añadido

--- APP-PANAP/src/app/1-cliente/registro-cliente/registro-cliente.page.ts
+++ APP-PANAP/src/app/1-cliente/registro-cliente/registro-cliente.page.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-registro-cliente',
   templateUrl: './registro-cliente.page.html',
   styleUrls: ['./registro-cliente.page.scss'],
 })
-export class RegistroClientePage implements OnInit {
+export class RegistroClientePage {
+  apiUrl = 'https://panapp.duckdns.org/rest/API_PRUEBA.php';
 
-  constructor() { }
+  nombre: string = '';
+  apellido: string = '';
+  clave: string = '';
+  correo: string = '';
+  tipo: string = 'Cliente';
 
-  ngOnInit() {
+  constructor(private http: HttpClient) {}
+
+  probarAPI() {
+    const body = {
+      nombre: this.nombre,
+      apellido: this.apellido,
+      clave: this.clave,
+      correo: this.correo,
+      tipo: this.tipo,
+    };
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': ''
+    });
+
+    this.http.post(this.apiUrl, body, { headers: headers })
+      .subscribe(response => {
+        console.log('Respuesta de la API:', response);
+      }, error => {
+        console.error('Error al consumir la API:', error);
+      });
   }
-
 }
