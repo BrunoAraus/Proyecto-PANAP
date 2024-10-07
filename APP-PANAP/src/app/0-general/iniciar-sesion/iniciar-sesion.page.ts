a401573fd0fdee21eb78a98a8e098c40dcc59bae
BrunoAraus <101360453+BrunoAraus@users.noreply.github.com>
2024-10-07 15:48:51 -0300

    Inicio de Sesion y Arreglos al registro
    Desarrollo del inicio de sesion y realizacion de arreglos minimos al apartado de registro
    
    Co-Authored-By: TheKeno247 <101360575+TheKeno247@users.noreply.github.com>

--- APP-PANAP/src/app/0-general/iniciar-sesion/iniciar-sesion.page.ts
+++ APP-PANAP/src/app/0-general/iniciar-sesion/iniciar-sesion.page.ts
@@ -1,15 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-iniciar-sesion',
   templateUrl: './iniciar-sesion.page.html',
   styleUrls: ['./iniciar-sesion.page.scss'],
 })
-export class IniciarSesionPage implements OnInit {
+export class IniciarSesionPage {
+  apiUrl = 'https://panapp.duckdns.org/rest/API_PRUEBA.php';
 
-  constructor() { }
+  correo: string = ''; 
+  clave: string = '';  
 
-  ngOnInit() {
+  errorMensaje: string = ''; 
+
+  constructor(private http: HttpClient, private navCtrl: NavController) {}
+
+  iniciarSesion() {
+    const body = {
+      accion: 'login',
+      correo: this.correo,
+      clave: this.clave
+    };
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': ''
+    });
+
+    this.http.post(this.apiUrl, body, { headers: headers })
+      .subscribe(
+        (response: any) => {
+          if (response.success) {
+            console.log('Login exitoso:', response.message);
+
+            this.errorMensaje = ''; 
+
+            
+            if (response.tipo === 'Cliente') {
+              
+              this.navCtrl.navigateRoot('/tabs-cliente/home-cliente');
+            } else if (response.tipo === 'Negocio'){
+              
+              this.navCtrl.navigateRoot('/tabs-negocio/home-negocio');
+            }
+
+          } else {
+            this.errorMensaje = response.message; 
+          }
+        },
+        (error) => {
+          console.error('Error al consumir la API:', error);
+          this.errorMensaje = 'Ocurrió un error inesperado. Inténtalo más tarde.';
+        }
+      );
   }
-
 }
