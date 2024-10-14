7002af06750887150ed8c6b39865f49c4c67d048
JoSeCalderon1234 <101206035+JoSeCalderon1234@users.noreply.github.com>
2024-10-14 20:02:12 -0300

    Correción visual y arreglo de Bugs en REGISTRO
    Falta:
    * Corregir varios errores visuales de Cliente y Negocio en general.

---
+++ APP-PANAP/src/app/0-general/confirmacion-negocio/confirmacion-negocio.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-confirmacion-negocio',
+  templateUrl: './confirmacion-negocio.page.html',
+  styleUrls: ['./confirmacion-negocio.page.scss'],
+})
+export class ConfirmacionNegocioPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
