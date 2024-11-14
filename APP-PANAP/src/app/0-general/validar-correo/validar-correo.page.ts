02567472b230ecbab48c0576950adcc8a179edcd
TheKeno247 <ma.florest@duocuc.cl>
2024-11-14 19:53:21 -0300

    Validacion Correo
    Ser realiza validacion para correos, de esta manera se revisa que tenga lo correspondiente a un correo (@, .cl - .com, ETC),

--- APP-PANAP/src/app/0-general/validar-correo/validar-correo.page.ts
+++ APP-PANAP/src/app/0-general/validar-correo/validar-correo.page.ts
@@ -10,6 +10,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class ValidarCorreoPage implements OnInit {
   correo: string = '';
+  errorCorreo: string = ''; // Mensaje de error para el correo
 
   ngOnInit() { 
   }
@@ -17,11 +18,19 @@ export class ValidarCorreoPage implements OnInit {
   
   constructor(private navCtrl: NavController, private http: HttpClient) {
   }
-  
-
+    // Validar el formato del correo
+    esCorreoValido(): boolean {
+      const correoRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+      return correoRegex.test(this.correo);
+    }
   validarCodigo() {
+    if (this.esCorreoValido()) {
+      // Si el correo es válido, guardarlo y navegar a la siguiente página
       localStorage.setItem('correo', this.correo);
       this.navCtrl.navigateRoot('/validar-codigo-contra');
+    } else {
+      // Si el correo no es válido, mostrar un mensaje de error
+      this.errorCorreo = 'Por favor, ingrese un correo electrónico válido';
+    }
   }
 }
-
